82f453c6f03388d3609431678b34a417f6fc750a
Mikołaj Sacha <mikolajsacha@gmail.com>
2018-06-08 12:55:24 +0200

    Set GPU memory restriction for tensorflow

--- train.py
+++ train.py
@@ -6,6 +6,15 @@ from src.datasets.cifar import load_cifar100
 from src.training.train import run_meta_learning
 
 
+# set some GPU memory restrictions for tensorflow and Keras
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
+
 def train(clear_logs: bool = False, description: str = ''):
     """
     Train Meta-Learner
