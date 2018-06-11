Fix reshaping problem when using hessian vectors

src/isotropy/lanczos.py
@@ -126,7 +126,7 @@ class TopKEigenvaluesBatched(Callback):
 
         def get_eigenvector_features():
             biggest_eigenval, biggest_eigenvec = self.compute_top(k=self.feature_K, with_vectors=True)
-            return [np.float32(biggest_eigenval), np.float32(biggest_eigenvec[:, 0])]
+            return [np.float32(biggest_eigenval), np.float32(biggest_eigenvec[:, :self.feature_K])]
 
         self.spectral_norm_tensor = tf.py_func(get_eigenvalue_feature, [], tf.float32)
 

src/model/meta_learner/meta_predict_model.py
@@ -209,7 +209,7 @@ class MetaPredictLearnerModel(Model, Optimizer):
                                            newshape=(1, len(inputs_history[3]), self.hessian_eigenvals))
             inputs_history[3] = np.repeat(inputs_history[3], axis=0, repeats=self.output_size)
         if len(inputs_history) > 4:
-            inputs_history[4] = np.expand_dims(np.transpose(inputs_history[4]), axis=-1)
+            inputs_history[4] = inputs_history[4].reshape(self.output_size, self.backprop_depth, self.hessian_eigenvals)
 
         # we need only final value of params_input for BPTT
         if self.backprop_depth != 1:
@@ -267,7 +267,7 @@ class MetaPredictLearnerModel(Model, Optimizer):
                                            newshape=(1, len(inputs_history[3]), self.hessian_eigenvals))
             inputs_history[3] = np.repeat(inputs_history[3], axis=0, repeats=self.output_size)
         if len(inputs_history) > 4:
-            inputs_history[4] = np.expand_dims(np.transpose(inputs_history[4]), axis=-1)
+            inputs_history[4] = inputs_history[4].reshape(self.output_size, self.backprop_depth, self.hessian_eigenvals)
 
         # we need only final value of params_input for BPTT
         if self.backprop_depth == 1: